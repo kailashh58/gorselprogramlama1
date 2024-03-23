19b3e8bd2252521ace366f04e62c0ea470dfe1e7
kailash <batuhangulpinar@hotmail.com>
2024-03-23 19:59:57 +0300

    satır silme

--- gorselprogramlama1/Form1.Designer.cs
+++ gorselprogramlama1/Form1.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///Gerekli tasarımcı değişkeni.
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///Kullanılan tüm kaynakları temizleyin.
-        /// </summary>
-        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
