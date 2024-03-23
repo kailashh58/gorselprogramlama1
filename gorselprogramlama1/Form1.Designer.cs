05dd0d00b7a4a50cb6f27ad904a49fe3332c3e71
kailash <batuhangulpinar@hotmail.com>
2024-03-23 20:34:13 +0300

    buttonların eklenmesi ve formların birbirine baglanması

--- gorselprogramlama1/Form1.Designer.cs
+++ gorselprogramlama1/Form1.Designer.cs
@@ -23,13 +23,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kitap ekleme";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(280, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 53);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Üye ekleme";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 53);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Kitap emanet işlemleri";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(261, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 59);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gümüş Kitapevi";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- gorselprogramlama1/Form1.cs
+++ gorselprogramlama1/Form1.cs
@@ -16,5 +16,29 @@ namespace gorselprogramlama1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.Show();
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
